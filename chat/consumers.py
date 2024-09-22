c0a69808b04af061146fef3a9c83427a1264471f
김병민 <gimbyeongmin@byeong.local>
2024-09-22 17:12:28 +0900

    base.html css update

--- chat/consumers.py
+++ chat/consumers.py
@@ -15,7 +15,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
         if waiting_users: 
-            # waiting_user에 있는 user가져오기 
+            # waiting_user에 있는 channel_name가져오기 
             self.partner_user = waiting_users.pop(0)
 
             self.group_name = f"{self.partner_user[-10:]}"
@@ -33,9 +33,11 @@ class ChatConsumer(AsyncWebsocketConsumer):
             await self.send(text_data=json.dumps({
                 "message": "대화 상대가 연결 됐습니다.",
                 "user_id": self.user_id,
+                "submitButton": False,
                 "send_user_id":0}))
             await self.channel_layer.send(self.partner_user, {"type": "chat.message", 
             "message": '대화 상대가 연결 됐습니다', 
+            "submitButton": False,
             "user_id": self.user_id, 
             "send_user_id":0})
             
@@ -48,13 +50,16 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.group_name = self.channel_name[-10:]
             await self.send(text_data=json.dumps({
                 "message": "대화 상대를 기다리고 있습니다.",
-                "user_id": self.user_id,"send_user_id":0}))
+                "submitButton": True,
+                "user_id": self.user_id,
+                "send_user_id":0}))
 
 
     async def disconnect(self, close_code):
-            if self.group_name in waiting_users:
-                waiting_users.remove(self.group_name)
-            
+            # 같은 이름이의 waiting_user 가있으면 제거
+            if self.channel_name in waiting_users:
+                waiting_users.remove(self.channel_name)
+
             await self.channel_layer.group_send(
             self.group_name,
                 {
@@ -68,15 +73,17 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     
     async def receive(self, text_data):
+        # 아직 대기중인 user일 경우 메시지 못보내게 방지
+        if self.channel_name in waiting_users:
+            return await self.send(text_data=json.dumps({
+                "message": "대화 상대를 기다리고 있습니다.",
+                "submitButton": True,
+                "user_id": self.user_id,
+                "send_user_id":0}))
+        
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
         sendUserId = text_data_json["sendUserId"]
-
-        #1분 간격으로 group의 상태를 redis에 업데이트
-        if int(time.time())-group_status[self.group_name] > 60:
-            group_status[self.group_name] = int(time.time())
-            cache.set(key='group_status',value=json.dumps(group_status))
-
 
         cache_key = f'{self.group_name}_{self.user_id} '
         # 중복요청과 도배성 message 방지
@@ -84,14 +91,29 @@ class ChatConsumer(AsyncWebsocketConsumer):
             return await self.send(text_data=json.dumps({
                 "message": "도배성 글 작성은 안됩니다.",
                 "user_id": self.user_id,
+                "submitButton": False,
                 "send_user_id":0}))
         
         cache.set(cache_key, self.user_id, timeout=1)
+
+        #1분 간격으로 group의 상태를 redis에 업데이트
+        if int(time.time())-group_status[self.group_name] > 60:
+            group_status[self.group_name] = int(time.time())
+            cache.set(key='group_status',value=json.dumps(group_status))
 
         #공백 message 처리
         if not message.replace(' ',''):
             return await self.send(text_data=json.dumps({
                 "message": "메시지를 입력해 주세요.",
+                "submitButton": False,
+                "user_id": self.user_id,
+                "send_user_id":0}))
+        
+        #메시지 50자 이하만 입력가능 
+        if len(message) > 50:
+            return await self.send(text_data=json.dumps({
+                "message": "50자 까지 입력 가능합니다.",
+                "submitButton": False,
                 "user_id": self.user_id,
                 "send_user_id":0}))
 
@@ -99,6 +121,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_send(
             self.group_name, {"type": "chat.message", 
             "message": [message],
+            "submitButton": False,
             "user_id": self.user_id, 
             "send_user_id":sendUserId}
         )
@@ -108,7 +131,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
         message = event["message"]
         sendUserId = event["send_user_id"]
         await self.send(text_data=json.dumps({
-            "message": [message], 
+            "message": [message],
+            "submitButton": False,
             "user_id":self.user_id,
             "send_user_id":sendUserId}))
 
@@ -116,6 +140,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
     # 화면 refresh 버튼 활성화 
     async def refresh_page(self, event):
         message = event["message"]
-        await self.send(text_data=json.dumps({"refresh": True, "message": [message], "send_user_id":0}))
+        await self.send(text_data=json.dumps({"refresh": True,"submitButton": True, "message": [message], "send_user_id":0}))
     
 
